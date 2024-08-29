163c3fb9aec80902413a30e53c782d2e0715bd61
Tony Hoong <tonyhoong40@gmail.com>
2024-08-29 15:56:16 +0100

    removed preline. updated all dependencies to their latest version. added the missing config files that were added in later versions of vite ts projects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{js,jsx,ts,tsx}", "node_modules/preline/dist/*.js"],
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
         extend: {},
     },
-    plugins: [require("preline/plugin"), require("@tailwindcss/forms")],
+    plugins: [require("@tailwindcss/forms")],
     darkMode: "class",
 };
